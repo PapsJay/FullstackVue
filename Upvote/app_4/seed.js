3f431b90a72088d087358880d6568ca8c4a0257e
PapsJay <donallotei@gmail.com>
2023-05-31 23:21:39 +0000

    added app-4 files to folder

---
+++ Upvote/app_4/seed.js
@@ -0,0 +1,14 @@
+window.Seed = (function () {
+    const submissions = [
+        {
+            id: 1,
+            title: 'Yellow Pail',
+            description: "On-demand sand castle construction expertise.",
+            url: '#',
+            votes: 16,
+            avatar: '../public/images/avatars/daniel.jpg',
+            submissionImage: '../public/images/submissions/image-yellow.png',
+        }
+    ];
+    return { submissions: submissions };
+}());
