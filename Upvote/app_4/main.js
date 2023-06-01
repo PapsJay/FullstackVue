4fe22f38f81111554a3ea44dd596a94b1e40d236
PapsJay <donallotei@gmail.com>
2023-06-01 18:45:24 +0000

    added an event handler and also a reactive state

--- Upvote/app_4/main.js
+++ Upvote/app_4/main.js
@@ -1,6 +1,21 @@
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    submissions: Seed.submissions 
-  }
+    submissions: Seed.submissions,
+  },
+  computed: {
+    sortedSubmissions() {
+      return this.submissions.sort((a, b) => {
+        return b.votes - a.votes;
+      });
+    },
+  },
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(
+        (submission) => submission.id === submissionId
+      );
+      submission.votes++;
+    },
+  },
 });
